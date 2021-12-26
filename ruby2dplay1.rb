b6b2bc4079a6eddf8d8470418442cce9c6531a11
Marat Oshmiansky <maratical@gmail.com>
2021-12-25 23:03:02 -0800

    getting better particle behavior

--- ruby2dplay1.rb
+++ ruby2dplay1.rb
@@ -10,27 +10,34 @@ num_of_lines.times do
   y_init = rand(0..480)
   x_offset = x_init + rand(-1..1)
   y_offset = y_init + rand(-1..1)
-  lines << Line.new(x1: x_init, y1: y_init, x2: x_offset, y2: y_offset, color: 'random')
+  lines << Line.new(x1: x_init, y1: y_init, x2: x_offset, y2: y_offset, color: 'white')
 end
 
 two_pi = 2 * Math::PI
 angle = 0
-x_mult = 6
-y_mult = x_mult
+angle_incr_mult = 0.001
+angle_incr = angle_incr_mult * Math::PI / 180
+x_sign = 1
+y_sign = 1
 
 update do
   lines.each do |line|
-    angle_mult = rand(0.001..0.01)
-    angle_incr = angle_mult * Math::PI / 180
+    x_mult = rand(1..20)
+    y_mult = rand(1..20)
+    x_angle_mult = rand(1..3)
+    y_angle_mult = rand(1..3)
     angle += angle_incr
     angle %= two_pi
-    x_move = x_mult * Math.cos(angle)
-    y_move = y_mult * Math.sin(angle)
-    # x_move_mult, y_move_mult = rand, rand
-    line.x1 += x_move
-    line.y1 += y_move
-    line.x2 += x_move
-    line.y2 += y_move
+    x_move = x_mult * Math.cos(x_angle_mult * angle)
+    y_move = y_mult * Math.sin(y_angle_mult * angle)
+
+    (line.x1 + x_move).between?(0, 640) ? x_sign = 1 : x_sign = -1
+    (line.y1 + y_move).between?(0, 480) ? y_sign = 1 : y_sign = -1
+    
+    line.x1 += x_sign * x_move
+    line.x2 += x_sign * x_move
+    line.y1 += y_sign * y_move
+    line.y2 += y_sign * y_move    
   end
 end
 
