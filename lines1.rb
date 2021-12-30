dc3c95cc90866d9e3c8db0bdacb1dbb3f4859fce
Marat Oshmiansky <maratical@gmail.com>
2021-12-30 02:39:25 -0800

    fixing x, y motion

--- lines1.rb
+++ lines1.rb
@@ -1,103 +1,74 @@
 require "ruby2d"
 
-set title: "Lines1"
-
-set width: 640, height: 600
+set width: 640, height: 600, title: "Lines1"
 
 # CONSTANTS
 NUM_OF_LINES = 25
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 100, 100
 X_LINE_LENGTH_BOUND, Y_LINE_LENGTH_BOUND = 40.0, 40.0
 MAX_LINES = 20 * NUM_OF_LINES
-X_MOVE_BOUND, Y_MOVE_BOUND = 1, 1
-X_SPEED, Y_SPEED = 0.2, 0.2
-X_MAX_SPEED, Y_MAX_SPEED = 4.0, 4.0
 
 class Line
-  # def x_move
-  #   @x_move ||= x_move_reset
-  # end
-
-  # def y_move
-  #   @y_move ||= y_move_reset
-  # end
-
-  # def x_accel
-  #   if @x_move.abs < X_MAX_SPEED
-  #     if @x_move >= 0
-  #       @x_move += rand(-X_SPEED..X_SPEED)
-  #     else
-  #       @x_move -= rand(-X_SPEED..X_SPEED)
-  #     end
-  #   else
-  #     x_move_reset
-  #   end
-  # end
-
-  # def y_accel
-  #   if @y_move.abs < Y_MAX_SPEED
-  #     if @y_move >= 0
-  #       @y_move += rand(-Y_SPEED..Y_SPEED)
-  #     else
-  #       @y_move -= rand(-Y_SPEED..Y_SPEED)
-  #     end
-  #   else
-  #     y_move_reset
-  #   end
-  # end
-
-  # def x_move_reset
-  #   @x_move = rand(-X_MOVE_BOUND..X_MOVE_BOUND)
-  # end
-
-  # def y_move_reset
-  #   @y_move = rand(-Y_MOVE_BOUND..Y_MOVE_BOUND)
-  # end
-
-  def x_edge_check
-    unless self.x1.between?(X_WINDOW_OFFSET, Window.width - X_WINDOW_OFFSET) || self.x2.between?(X_WINDOW_OFFSET, Window.width - X_WINDOW_OFFSET)
-      @x_move = -@x_move
-      # color_swap
-    end
+  def x_moved_last
+    x_moved_reset unless @x_moved_last
   end
 
-  def y_edge_check
-    unless self.y1.between?(Y_WINDOW_OFFSET, Window.height - Y_WINDOW_OFFSET) || self.y2.between?(Y_WINDOW_OFFSET, Window.height - Y_WINDOW_OFFSET)
-      @y_move = -@y_move
-      # color_swap
-    end
+  def y_moved_last
+    y_moved_reset unless @y_moved_last
   end
 
-  # def color_swap
-  #   self.color = ["white", "fuchsia", "yellow"].sample
-  # end
+  def x_moved_reset
+    @x_moved_last = true
+    @y_moved_last = false
+  end
+
+  def y_moved_reset
+    @x_moved_last = false
+    @y_moved_last = true
+  end
 end
 
 grid_square = Math.sqrt(NUM_OF_LINES).floor
-x_viewport_adj = (Window.width - X_WINDOW_OFFSET * 2)
-y_viewport_adj = (Window.height - Y_WINDOW_OFFSET * 2)
+x_viewport_adj = (Window.width - X_WINDOW_OFFSET)
+y_viewport_adj = (Window.height - Y_WINDOW_OFFSET)
 lines, new_lines = [], []
 
-# set up line grid
+# set up point grid
 grid_square.times do |i|
   grid_square.times do |j|
-    x1_init = X_WINDOW_OFFSET + grid_square * (i + 1) * x_viewport_adj / NUM_OF_LINES
-    y1_init = Y_WINDOW_OFFSET + grid_square * (j + 1) * y_viewport_adj / NUM_OF_LINES
-    x2_init = x1_init + rand(-X_LINE_LENGTH_BOUND..X_LINE_LENGTH_BOUND)
-    y2_init = y1_init + rand(-Y_LINE_LENGTH_BOUND..Y_LINE_LENGTH_BOUND)
+    x1_init = X_WINDOW_OFFSET + grid_square * i * x_viewport_adj / NUM_OF_LINES
+    y1_init = Y_WINDOW_OFFSET + grid_square * j * y_viewport_adj / NUM_OF_LINES
+    x2_init = x1_init + 1
+    y2_init = y1_init + 1
 
     lines << Line.new(x1: x1_init, y1: y1_init, x2: x2_init, y2: y2_init, width: 1, color: "white")
   end
+end
+
+lines.each do |line|
+  [line.x_moved_reset, line.y_moved_reset].sample
 end
 
 all_lines = lines
 
 (MAX_LINES / NUM_OF_LINES).times do
   lines.each do |line|
-    x1_new = line.x2
-    y1_new = line.y2
-    x2_new = x1_new + rand(-X_LINE_LENGTH_BOUND..X_LINE_LENGTH_BOUND)
-    y2_new = y1_new + rand(-Y_LINE_LENGTH_BOUND..Y_LINE_LENGTH_BOUND)
+    if line.y_moved_last
+      x1_new = line.x2
+      x2_new = x1_new + rand(-X_LINE_LENGTH_BOUND..X_LINE_LENGTH_BOUND)
+      y1_new = line.y2
+      y2_new = line.y2
+      line.x_moved_reset
+    elsif line.x_moved_last
+      x1_new = line.x2
+      x2_new = line.x2
+      y1_new = line.y2
+      y2_new = y1_new + rand(-Y_LINE_LENGTH_BOUND..Y_LINE_LENGTH_BOUND)
+      line.y_moved_reset
+    else
+      puts "oopsie"
+    end
+
     new_lines << Line.new(x1: x1_new, y1: y1_new, x2: x2_new, y2: y2_new, width: 1, color: "white")
   end
 
