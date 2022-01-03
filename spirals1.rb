c284d4306853541d6fc75e144e21f120828fc3b8
Marat Oshmiansky <maratical@gmail.com>
2022-01-02 23:28:07 -0800

    modify multipliers

--- spirals1.rb
+++ spirals1.rb
@@ -3,7 +3,7 @@ require "ruby2d"
 set width: 650, height: 600, title: "Spirals!"
 
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 75, 75
-X_MULT, Y_MULT = 1, 1
+X_MULT, Y_MULT = 3, 3
 DEGS_TO_RADIANS = Math::PI / 180
 ANGLE_INCR = 6
 RADIUS_INCR = 0.01
@@ -38,14 +38,14 @@ def draw_segment(x1, y1, x2, y2, line_color)
   Line.new(x1: x1, y1: y1, x2: x2, y2: y2, width: 1, color: line_color)
 end
 
-gradients = [%w(white yellow orange red), %w(white aqua teal blue), %W(white fuchsia maroon purple), %W(white lime green olive)]
+gradients = [%w(white yellow orange red), %w(white aqua teal blue)]
 gradient = gradients.sample
-# how to eliminate the need for this code below (which is duplicated in the if statement in the update loop)?
+# how to avoid duplicating this code in the update loop?
 line_color = gradient.sample
 line = line_init(line_color)
 angle, radius = 0, 0
 radius_incr = RADIUS_INCR
-x_angle_mult = rand(9..27)
+x_angle_mult = rand(9..44)
 y_angle_mult = x_angle_mult
 Text.new("x_angle_mult = #{x_angle_mult}", x: Window.width / 2 - 150)
 start = false
@@ -56,7 +56,7 @@ update do
     line = line_init(line_color)
     angle, radius = 0, 0
     radius_incr = RADIUS_INCR
-    x_angle_mult = rand(9..27)
+    x_angle_mult = rand(9..44)
     y_angle_mult = x_angle_mult
     Text.new("x_angle_mult = #{x_angle_mult}", x: Window.width / 2 - 150)
     start = false
