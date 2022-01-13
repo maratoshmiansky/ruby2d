550b2c59ac5a591c80f82264ebad8485273f51ca
Marat Oshmiansky <maratical@gmail.com>
2022-01-12 16:48:48 -0800

    make init an instance method; move all distance calculations to get_distance; remove attr_accessor

--- particles6.rb
+++ particles6.rb
@@ -10,9 +10,14 @@ X_CENTER, Y_CENTER = Window.width / 2, Window.height / 2
 MOVE_MULT = 5
 
 class Point < Square
-  attr_accessor :x_init, :y_init, :distance_init, :x_distance_init, :y_distance_init
+  def animate
+    get_distance
+    move
+  end
 
   def get_distance
+    @x_distance = X_CENTER - self.x
+    @y_distance = Y_CENTER - self.y
     @distance = Math.sqrt(@x_distance ** 2 + @y_distance ** 2)
   end
 
@@ -21,11 +26,12 @@ class Point < Square
     self.y += MOVE_MULT * (@y_distance + @y_distance_init) / (@distance + @distance_init)
   end
 
-  def animate
-    @x_distance = X_CENTER - self.x
-    @y_distance = Y_CENTER - self.y
-    get_distance
-    move
+  def init
+    @x_init = self.x
+    @y_init = self.y
+    @x_distance_init = X_CENTER - @x_init
+    @y_distance_init = Y_CENTER - @y_init
+    @distance_init = Math.sqrt(@x_distance_init ** 2 + @y_distance_init ** 2)
   end
 end
 
@@ -41,11 +47,7 @@ X_NUM_OF_POINTS.times do |i|
 end
 
 points.each do |point|
-  point.x_init = point.x
-  point.y_init = point.y
-  point.x_distance_init = X_CENTER - point.x_init
-  point.y_distance_init = Y_CENTER - point.y_init
-  point.distance_init = Math.sqrt(point.x_distance_init ** 2 + point.y_distance_init ** 2)
+  point.init
 end
 
 update do
