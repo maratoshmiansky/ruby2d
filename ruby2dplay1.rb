1cac91ff98494c380bcfd44f7a6e82e0e3222626
Marat Oshmiansky <maratical@gmail.com>
2022-01-16 16:32:15 -0800

    fix angle calculations, change some multipliers

--- ruby2dplay1.rb
+++ ruby2dplay1.rb
@@ -1,44 +1,35 @@
 require "ruby2d"
 
-set width: 650, height: 600, title: "Particles!"
+set width: 600, height: 600, title: "Particles!"
 
-TWO_PI = 2 * Math::PI
 DEGS_TO_RADIANS = Math::PI / 180
-ANGLE_INCR_MULT = 0.0003
+ANGLE_INCR_MULT = 0.0005
 
-num_of_lines = 1000
-lines = []
+num_of_points = 1000
+points = []
 
-num_of_lines.times do
-  x_init = rand(0..Window.width)
-  y_init = rand(0..Window.height)
-  x_offset = x_init + 1
-  y_offset = y_init + 1
-  lines << Line.new(x1: x_init, y1: y_init, x2: x_offset, y2: y_offset, color: "white")
+num_of_points.times do
+  x_init = rand(0.0..Window.width)
+  y_init = rand(0.0..Window.height)
+  points << Square.new(x: x_init, y: y_init, size: 1, color: "white")
 end
 
 angle = 0
-angle_incr = ANGLE_INCR_MULT * DEGS_TO_RADIANS
 x_sign, y_sign = 1, 1
 
 update do
-  lines.each do |line|
-    x_mult = rand(1..20)
-    y_mult = rand(1..20)
-    x_angle_mult = rand(1..3)
-    y_angle_mult = rand(1..3)
-    angle += angle_incr
-    angle %= TWO_PI
-    x_move = x_mult * Math.cos(x_angle_mult * angle)
-    y_move = y_mult * Math.sin(y_angle_mult * angle)
-
-    (line.x1 + x_move).between?(0, Window.width) ? x_sign = 1 : x_sign = -1
-    (line.y1 + y_move).between?(0, Window.height) ? y_sign = 1 : y_sign = -1
-
-    line.x1 += x_sign * x_move
-    line.x2 += x_sign * x_move
-    line.y1 += y_sign * y_move
-    line.y2 += y_sign * y_move
+  points.each do |point|
+    x_mult = rand(1.0..50.0)
+    y_mult = rand(1.0..50.0)
+    x_angle_mult = rand(1.0..2.0)
+    y_angle_mult = rand(1.0..2.0)
+    angle = (angle + ANGLE_INCR_MULT) % 360
+    x_move = x_mult * Math.cos(x_angle_mult * angle * DEGS_TO_RADIANS)
+    y_move = y_mult * Math.sin(y_angle_mult * angle * DEGS_TO_RADIANS)
+    (point.x + x_move).between?(0, Window.width) ? x_sign = 1 : x_sign = -1
+    (point.y + y_move).between?(0, Window.height) ? y_sign = 1 : y_sign = -1
+    point.x += x_sign * x_move
+    point.y += y_sign * y_move
   end
 end
 
