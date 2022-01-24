b06af6f912555d4c69e8e743ce5626390bafa78d
Marat Oshmiansky <maratical@gmail.com>
2022-01-23 16:09:34 -0800

    change filename, change each_cons to each_slice so circles grow and shrink correctly

--- snail_swirl1.rb
+++ snail_swirl1.rb
@@ -1,6 +1,6 @@
 require "ruby2d"
 
-set width: 600, height: 600, background: "white", title: "Flower Power"
+set width: 600, height: 600, background: "white", title: "Snailshell Swirl"
 
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 60, 60
 X_CENTER, Y_CENTER = Window.width / 2, Window.height / 2
@@ -9,37 +9,33 @@ ANGLE_DELTA = 12
 CIRCLE_RADIUS_MIN = 10.0
 CIRCLE_RADIUS_MAX = 60.0
 CIRCLE_RADIUS_DELTA = 1.01
-FLOWER_RADIUS_MIN = 1.0
-FLOWER_RADIUS_DELTA = 1.08 #1.015
-NUM_OF_CIRCLES = 60
+FLOWER_RADIUS_MIN = 5.0
+FLOWER_RADIUS_DELTA = 1.02
+NUM_OF_CIRCLES = 180
 
 class Circle
   attr_reader :growing
 
   def grow
-    if @radius <= CIRCLE_RADIUS_MAX
-      @radius *= CIRCLE_RADIUS_DELTA
-    else
+    @radius *= CIRCLE_RADIUS_DELTA
+  end
+
+  def shrink
+    @radius /= CIRCLE_RADIUS_DELTA
+  end
+
+  def set_growing
+    if @radius < [@radius_init, CIRCLE_RADIUS_MIN].min
+      @growing = true
+    elsif @radius > [@radius_fin, CIRCLE_RADIUS_MAX].max
       @growing = false
     end
   end
 
-  def shrink
-    if @radius >= CIRCLE_RADIUS_MIN
-      @radius /= CIRCLE_RADIUS_DELTA
-    else
-      @growing = true
-    end
-  end
-
-  def check_if_growing
-    @radius <= CIRCLE_RADIUS_MAX ? @growing = true : @growing = false
-  end
-
   def init
-    @x_init = @x
-    @y_init = @y
-    check_if_growing
+    @radius_init = @radius
+    @radius_fin = @radius * 2.0
+    @radius < @radius_fin ? @growing = true : @growing = false
   end
 end
 
@@ -51,6 +47,8 @@ flower_radius = FLOWER_RADIUS_MIN
 angle = 0
 
 NUM_OF_CIRCLES.times do
+  # x_coord = rand(X_WINDOW_OFFSET..Window.width - X_WINDOW_OFFSET)
+  # y_coord = rand(Y_WINDOW_OFFSET..Window.height - Y_WINDOW_OFFSET)
   x_coord = x_init + flower_radius * Math.cos(angle * DEGS_TO_RADS)
   y_coord = y_init + flower_radius * Math.sin(angle * DEGS_TO_RADS)
   circles << Circle.new(x: x_coord, y: y_coord, z: z_depth, radius: circle_radius, color: "navy")
@@ -61,23 +59,21 @@ NUM_OF_CIRCLES.times do
   z_depth += 2
 end
 
-circles.each do |circle|
-  circle.init
+circles.each_slice(2) do |circle|
+  circle[0].init
 end
 
 update do
-  circles.each_cons(2) do |circle|
+  circles.each_slice(2) do |circle|
     if circle[0].growing
       circle[0].grow
-    else
-      circle[0].shrink
-    end
-
-    if circle[1].growing
       circle[1].grow
     else
+      circle[0].shrink
       circle[1].shrink
     end
+
+    circle[0].set_growing
   end
 end
 
