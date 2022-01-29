78b06072d50e2b8734d634ee053595873b9c7256
Marat Oshmiansky <maratical@gmail.com>
2022-01-29 00:25:10 -0800

    fix angle calculation, increase number of circles, slow rotation

--- circle_play2.rb
+++ circle_play2.rb
@@ -5,16 +5,15 @@ set width: 600, height: 600, background: "white", title: "Circle Play"
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 60, 60
 X_CENTER, Y_CENTER = Window.width / 2, Window.height / 2
 DEGS_TO_RADS = Math::PI / 180
-CIRCLE_RADIUS_INIT = 6.0
-CIRCLE_RADIUS_DELTA = 1.18
+CIRCLE_RADIUS_INIT = 3.0
+CIRCLE_RADIUS_DELTA = 1.1
 CIRCLE_BORDER_MULT = 0.75
-NUM_OF_CIRCLES_INIT = 12
-NUM_OF_CIRCLES_DELTA = 1.0
-ANGLE_DELTA = 360 / NUM_OF_CIRCLES_INIT
-RING_RADIUS_INIT = 40.0
+NUM_OF_CIRCLES = 16
+ANGLE_DELTA = 360.0 / NUM_OF_CIRCLES
+RING_RADIUS_INIT = 10.0
 RING_RADIUS_DELTA = 1.15
-NUM_OF_RINGS = 14
-ROT_ANGLE_DIV = 30
+NUM_OF_RINGS = 24
+ROT_ANGLE_DIV = 60
 
 class Circle
   attr_reader :radius, :growing
@@ -48,13 +47,12 @@ x_init, y_init = X_CENTER, Y_CENTER
 z_depth = 0
 circle_radius = CIRCLE_RADIUS_INIT
 ring_radius = RING_RADIUS_INIT
-num_of_circles = NUM_OF_CIRCLES_INIT
 
 NUM_OF_RINGS.times do |num|
-  angle = 0
+  angle = ANGLE_DELTA
   num.odd? ? circle_color = "green" : circle_color = "fuchsia"
 
-  num_of_circles.times do
+  NUM_OF_CIRCLES.times do
     x_coord = x_init + ring_radius * Math.cos(angle * DEGS_TO_RADS)
     y_coord = y_init + ring_radius * Math.sin(angle * DEGS_TO_RADS)
     circles << Circle.new(x: x_coord, y: y_coord, z: z_depth, radius: circle_radius, color: "black")
@@ -65,7 +63,6 @@ NUM_OF_RINGS.times do |num|
 
   ring_radius *= RING_RADIUS_DELTA
   circle_radius *= CIRCLE_RADIUS_DELTA
-  num_of_circles = (num_of_circles + NUM_OF_CIRCLES_DELTA).ceil
 end
 
 circles.each_slice(2) do |circle|
