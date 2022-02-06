b757071f8b556d4f39935dabeec79ac8680a176f
Marat Oshmiansky <maratical@gmail.com>
2022-02-05 23:59:31 -0800

    eliminate phase change, revise some constants

--- circle_play3.rb
+++ circle_play3.rb
@@ -2,7 +2,7 @@ require "ruby2d"
 
 set width: 600, height: 600, background: "white", title: "Circle Play"
 
-X_NUM_OF_CIRCLES, Y_NUM_OF_CIRCLES = 24, 8
+X_NUM_OF_CIRCLES, Y_NUM_OF_CIRCLES = 32, 8
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 60, 60
 VIEWPORT_WIDTH = (Window.width - X_WINDOW_OFFSET * 2)
 VIEWPORT_HEIGHT = (Window.height - Y_WINDOW_OFFSET * 2)
@@ -10,37 +10,16 @@ X_CENTER, Y_CENTER = Window.width / 2, Window.height / 2
 X_GRID = VIEWPORT_WIDTH / X_NUM_OF_CIRCLES
 DEGS_TO_RADS = Math::PI / 180
 ANGLE_DELTA = 3.0
-ANGLE_DIST_DELTA_MIN = -2.0
-ANGLE_DIST_DELTA_MAX = 2.0
-ANGLE_DIST_DELTA = 0.01
-Y_AMP = 100.0
-CIRCLE_RADIUS_INIT = 8.0
+ANGLE_DIV = 1.5
+Y_AMP = 60.0
+CIRCLE_RADIUS_INIT = 6.0
 CIRCLE_BORDER_MULT = 0.75
 
 class Circle
-  attr_reader :angle_div_growing
-
   def wave
-    @angle = (X_CENTER - @x_init) * @angle_dist_delta
     @angle = (@angle + ANGLE_DELTA) % 360
     @y = @y_init + Y_AMP * Math.sin(@angle * DEGS_TO_RADS)
     @radius = @radius_init * (@y_init - @y).abs / Y_AMP
-  end
-
-  def angle_dist_delta_increment
-    @angle_dist_delta += ANGLE_DIST_DELTA
-  end
-
-  def angle_dist_delta_decrement
-    @angle_dist_delta -= ANGLE_DIST_DELTA
-  end
-
-  def set_angle_div_growing
-    if @angle_dist_delta < ANGLE_DIST_DELTA_MIN
-      @angle_div_growing = true
-    elsif @angle_dist_delta > ANGLE_DIST_DELTA_MAX
-      @angle_div_growing = false
-    end
   end
 
   def init
@@ -48,8 +27,7 @@ class Circle
     @x_distance_init = X_CENTER - @x_init
     # @y_distance_init = Y_CENTER - @y_init
     # @distance_init = Math.sqrt(@x_distance_init ** 2 + @y_distance_init ** 2)
-    @angle_dist_delta = ANGLE_DIST_DELTA_MIN
-    @angle = @x_distance_init / @angle_dist_delta
+    @angle = @x_distance_init / ANGLE_DIV
     @radius_init = @radius
   end
 end
@@ -73,14 +51,7 @@ end
 
 update do
   circles.each do |circle|
-    if circle.angle_div_growing
-      circle.angle_dist_delta_increment
-    else
-      circle.angle_dist_delta_decrement
-    end
-
     circle.wave
-    circle.set_angle_div_growing
   end
 end
 
