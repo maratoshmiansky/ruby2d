e0746884390f319e0951407912682f146e35e8a8
Marat Oshmiansky <maratical@gmail.com>
2022-02-06 23:40:49 -0800

    add two more waves, revise some constants

--- circle_play3.rb
+++ circle_play3.rb
@@ -3,18 +3,20 @@ require "ruby2d"
 set width: 600, height: 600, background: "white", title: "Circle Play"
 
 NUM_OF_CIRCLES = 36
+NUM_OF_WAVES = 3
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 60, 60
 VIEWPORT_WIDTH = (Window.width - X_WINDOW_OFFSET * 2)
 VIEWPORT_HEIGHT = (Window.height - Y_WINDOW_OFFSET * 2)
 X_CENTER, Y_CENTER = Window.width / 2, Window.height / 2
 X_GRID = VIEWPORT_WIDTH / NUM_OF_CIRCLES
+Y_GRID = VIEWPORT_HEIGHT / NUM_OF_WAVES
 DEGS_TO_RADS = Math::PI / 180
 ANGLE_DELTA = 4.0
 ANGLE_DIV = 1.5
 Y_AMP = 80.0
-CIRCLE_RADIUS_INIT = 6.0
+CIRCLE_RADIUS_INIT = 8.0
 CIRCLE_BORDER_MULT = 0.8
-RADIUS_DIV = 5.0
+RADIUS_DIV = 4.0
 
 class Circle
   def wave
@@ -31,15 +33,17 @@ class Circle
 end
 
 circles = []
-y_coord = Y_CENTER
 z_depth = 0
 
-NUM_OF_CIRCLES.times do |i|
-  i.odd? ? circle_color = "green" : circle_color = "fuchsia"
-  x_coord = X_WINDOW_OFFSET + (i + 0.5) * X_GRID
-  circles << Circle.new(x: x_coord, y: y_coord, z: z_depth, radius: CIRCLE_RADIUS_INIT, color: "black")
-  circles << Circle.new(x: x_coord, y: y_coord, z: z_depth + 1, radius: CIRCLE_RADIUS_INIT * CIRCLE_BORDER_MULT, color: circle_color)
-  z_depth += 2
+NUM_OF_WAVES.times do |j|
+  NUM_OF_CIRCLES.times do |i|
+    i.odd? ? circle_color = "green" : circle_color = "fuchsia"
+    x_coord = X_WINDOW_OFFSET + (i + 0.5) * X_GRID
+    y_coord = Y_WINDOW_OFFSET + (j + 0.5) * Y_GRID
+    circles << Circle.new(x: x_coord, y: y_coord, z: z_depth, radius: CIRCLE_RADIUS_INIT, color: "black")
+    circles << Circle.new(x: x_coord, y: y_coord, z: z_depth + 1, radius: CIRCLE_RADIUS_INIT * CIRCLE_BORDER_MULT, color: circle_color)
+    z_depth += 2
+  end
 end
 
 circles.each do |circle|
