fc575423561594543f77bba8b8fc30d3365d090d
Marat Oshmiansky <maratical@gmail.com>
2022-03-09 22:39:46 -0800

    add X_MOVE_BOUND and Y_MOVE_BOUND, resize window

--- particles1.rb
+++ particles1.rb
@@ -1,8 +1,9 @@
 require "ruby2d"
 
-set title: "Particles!"
+set title: "Particles!", width: 600, height: 600
 
 NUM_OF_POINTS = 5000
+X_MOVE_BOUND, Y_MOVE_BOUND = 2.0, 2.0
 
 class Point < Square
   attr_reader :x_move, :y_move
@@ -13,22 +14,22 @@ class Point < Square
   end
 
   def x_move_reset
-    @x_move = rand(-2.0..2.0)
+    @x_move = rand(-X_MOVE_BOUND..X_MOVE_BOUND)
   end
 
   def y_move_reset
-    @y_move = rand(-2.0..2.0)
+    @y_move = rand(-Y_MOVE_BOUND..Y_MOVE_BOUND)
   end
 
   def x_bounce
-    unless @x.between?(0, 640)
+    unless @x.between?(0, Window.width)
       @x_move = -@x_move
       color_swap
     end
   end
 
   def y_bounce
-    unless @y.between?(0, 480)
+    unless @y.between?(0, Window.height)
       @y_move = -@y_move
       color_swap
     end
@@ -47,7 +48,7 @@ end
 points = []
 
 NUM_OF_POINTS.times do
-  x_init, y_init = rand(0..640), rand(0..480)
+  x_init, y_init = rand(0..Window.width), rand(0..Window.height)
   points << Point.new(x: x_init, y: y_init, size: 1, color: "white")
 end
 
