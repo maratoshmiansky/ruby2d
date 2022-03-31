363bf788e4cbb6eb062be41cadbf8f88215fcaf0
Marat Oshmiansky <maratical@gmail.com>
2022-03-30 23:54:36 -0700

    change point grid to cannon-style emitter, rewrite x_speed_reset and y_speed_reset methods to match particles10.rb

--- particles2.rb
+++ particles2.rb
@@ -4,15 +4,13 @@ set title: "Particles!"
 
 set width: 600, height: 600
 
-X_NUM_OF_POINTS, Y_NUM_OF_POINTS = 6, 6
+NUM_OF_POINTS = 36
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 60, 60
-VIEWPORT_WIDTH = (Window.width - X_WINDOW_OFFSET * 2)
-VIEWPORT_HEIGHT = (Window.height - Y_WINDOW_OFFSET * 2)
-X_GRID = VIEWPORT_WIDTH / X_NUM_OF_POINTS
-Y_GRID = VIEWPORT_HEIGHT / Y_NUM_OF_POINTS
+X_CENTER, Y_CENTER = Window.width / 2, Window.height / 2
+X_CENTER_OFFSET, Y_CENTER_OFFSET = 10.0, 10.0
 X_SPEED, Y_SPEED = 0.2, 0.5
 X_SPEED_MIN, Y_SPEED_MIN = 0.0, 0.0
-X_SPEED_MAX, Y_SPEED_MAX = 12.0, 12.0
+X_SPEED_MAX, Y_SPEED_MAX = 2.0, 20.0
 
 class Point < Square
   def move
@@ -30,18 +28,18 @@ class Point < Square
 
   def y_accel
     if @y_speed <= Y_SPEED_MAX
-      @y_speed += rand(0.0..Y_SPEED)
+      @y_speed += Y_SPEED
     else
       y_speed_reset
     end
   end
 
   def x_speed_reset
-    @x_speed = X_SPEED_MIN
+    @x_speed = rand(-X_SPEED_MAX..X_SPEED_MAX)
   end
 
   def y_speed_reset
-    @y_speed = Y_SPEED_MIN
+    @y_speed = rand(Y_SPEED_MIN..Y_SPEED_MAX)
   end
 
   def x_edge_check
@@ -80,13 +78,10 @@ end
 
 points = []
 
-# set up point grid
-X_NUM_OF_POINTS.times do |i|
-  Y_NUM_OF_POINTS.times do |j|
-    x_init = X_WINDOW_OFFSET + (i + 0.5) * X_GRID
-    y_init = Y_WINDOW_OFFSET + (j + 0.5) * Y_GRID
-    points << Point.new(x: x_init, y: y_init, size: 1, color: "white")
-  end
+NUM_OF_POINTS.times do
+  x_init = rand(X_CENTER - X_CENTER_OFFSET..X_CENTER + X_CENTER_OFFSET)
+  y_init = rand(Y_CENTER - Y_CENTER_OFFSET..Y_CENTER + Y_CENTER_OFFSET)
+  points << Point.new(x: x_init, y: y_init, size: 1, color: "white")
 end
 
 points.each do |point|
@@ -95,12 +90,9 @@ end
 
 update do
   points.each do |point|
-    # bounce?
     point.x_edge_check
     point.y_edge_check
-    # move
     point.move
-    # speed up?
     point.x_accel
     point.y_accel
   end
