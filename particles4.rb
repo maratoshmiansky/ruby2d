d87990a0776dc80df57e667494627b39d515625c
Marat Oshmiansky <maratical@gmail.com>
2022-07-12 23:56:56 -0700

    Revert ternary to if-else in #expand in Point class

--- particles4.rb
+++ particles4.rb
@@ -44,7 +44,11 @@ class Point < Square
     @x_distance, @y_distance = @x_init - @x, @y_init - @y
     get_distance
 
-    @distance.between?(DISTANCE_MIN, @distance_init) ? move : @contracting = true
+    if @distance.between?(DISTANCE_MIN, @distance_init)
+      move
+    else
+      @contracting = true
+    end
   end
 
   def get_distance
