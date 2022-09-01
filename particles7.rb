c07607c8f36c8274f7a0475315bf4ab812e7bf30
Marat Oshmiansky <maratical@gmail.com>
2022-08-31 23:58:12 -0700

    Make background color (black) explicit; toggle particle color between fuchsia and green

--- particles7.rb
+++ particles7.rb
@@ -1,6 +1,6 @@
 require "ruby2d"
 
-set width: 600, height: 600, title: "Particles!"
+set width: 600, height: 600, background: "black", title: "Particles!"
 
 X_NUM_OF_POINTS, Y_NUM_OF_POINTS = 10, 10
 X_WINDOW_OFFSET, Y_WINDOW_OFFSET = 90, 90
@@ -67,7 +67,8 @@ X_NUM_OF_POINTS.times do |i|
   Y_NUM_OF_POINTS.times do |j|
     x_init = X_WINDOW_OFFSET + (i + 0.5) * X_GRID
     y_init = Y_WINDOW_OFFSET + (j + 0.5) * Y_GRID
-    points << Point.new(x: x_init, y: y_init, size: 1, color: "white")
+    color = (i + j).even? ? "fuchsia" : "green"
+    points << Point.new(x: x_init, y: y_init, size: 1, color: color)
   end
 end
 
